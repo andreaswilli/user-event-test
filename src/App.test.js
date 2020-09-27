a137bebb7f8f31cdf1acb473e236aff55669bde4
Andreas Willi <andiwilli8460@gmail.com>
2020-09-27 20:44:07 +0200

    Update testing library and add test example

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("log key codes", () => {
+  const handleKeyUp = (event) => {
+    console.log({ key: event.key, keyCode: event.keyCode });
+  };
+  render(<input type="text" onKeyUp={handleKeyUp} />);
+
+  const input = screen.getByRole("textbox");
+  userEvent.type(input, ".");
+  userEvent.type(input, "{del}");
 });
